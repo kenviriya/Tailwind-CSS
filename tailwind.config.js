2032fd23dba39de253929e113ff925a6522e35f4
kenviriya <kenviriya3010@gmail.com>
2022-05-10 02:38:46 +0700

    belajar tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#8A3FFC",
+        secondary: "#64758b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
